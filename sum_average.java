a83258171bcbe4981359603d9b2605f40e4eb68d
Sridipto Saha <sridiptosaha.enthu70@gmail.com>
2026-08-24 21:45:30 +0530

    Modify sum_average to read input from user
    Updated the main method to accept user input for numbers.

--- sum_average.java
+++ sum_average.java
@@ -1,4 +1,6 @@
+//Calculating sum and average using function argument.
 package Chapter_1;
+import java.util.Scanner;
 public class sum_average {
     public static void sum_average(int a, int b, int c) {
         int sum = a+b+c;
@@ -7,6 +9,13 @@ public class sum_average {
         System.out.println("The average is: "+avg);
     }
     public static void main(String [] args){
-        sum_average(16,19,28);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st number: ");
+        int a=sc.nextInt();
+        System.out.println("Enter 2nd number: ");
+        int b=sc.nextInt();
+        System.out.println("Enter 3rd number: ");
+        int c=sc.nextInt();
+        sum_average(a,b,c);
     }
 }
