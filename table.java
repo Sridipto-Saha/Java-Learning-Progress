fa17096ed0654898cf6ddb1938efdfa706a8fe93
Sridipto Saha <sridiptosaha.enthu70@gmail.com>
2026-09-05 21:33:26 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,13 @@
+package Hackerrank;
+import java.util.Scanner;
+public class table {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        for(int i=1;i<=10;i++){
+            int result = n*i;
+            System.out.println(n + " x " + i + " = " + result);
+        }
+    }
+}
